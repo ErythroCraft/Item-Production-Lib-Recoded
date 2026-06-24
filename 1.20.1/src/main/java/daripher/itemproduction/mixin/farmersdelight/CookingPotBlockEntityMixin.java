7bdcbf20c69fc9056bd5fb2975a2b86cf867caa3
Hinterix <76217862+ErythroCraft@users.noreply.github.com>
2026-06-24 10:00:43 +0200

    Add comment to CookingPotBlockEntityMixin
    Added a comment to prevent static and injection crashes in the modpack.

--- 1.20.1/src/main/java/daripher/itemproduction/mixin/farmersdelight/CookingPotBlockEntityMixin.java
+++ 1.20.1/src/main/java/daripher/itemproduction/mixin/farmersdelight/CookingPotBlockEntityMixin.java
@@ -6,4 +6,5 @@ import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
 @Mixin(CookingPotBlockEntity.class)
 public class CookingPotBlockEntityMixin {
   // LEER: Verhindert jegliche static- und Injektions-Abstürze im Modpack!
+  // In "ItemProductionLib.java" aufgenommen.
 }
